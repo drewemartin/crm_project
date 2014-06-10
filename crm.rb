23a0350c428fe5cef1b30ae1f6599a7aa0b56853
Drew Martin <drew.e.martin@gmail.com>
2014-06-10 17:17:41 -0400

    CRM completed

--- crm.rb
+++ crm.rb
@@ -1,5 +1,8 @@
 require 'sinatra'
 require_relative 'contact_class'
+require_relative 'rolodex_class'
+
+@@rolodex = Rolodex.new
 
 get '/' do 
 	@crm_app_name = "Drew Martin"
@@ -7,13 +10,16 @@ get '/' do
 	erb :index
 end
 
-# get '/contacts'
+get '/contacts' do
+	erb :contacts
+end
 
-# end
 
-# get '/contacts/new' #add new contact
 
-# end
+get '/contacts/new' do
+
+	erb :add_contacts
+end
 
 # get '/contacts/:id' #view a contact, this choice has to come last since it could overwrite others
 
@@ -22,3 +28,10 @@ end
 # get '/contacts/:id/edit' #find and edit unique contact
 
 # end
+
+post '/contacts' do
+puts params
+new_contact = Contact.new(params[:first_name], params[:last_name], params[:email_address], params[:notes])
+@@rolodex.add_contact(new_contact)
+redirect '/contacts'
+end
